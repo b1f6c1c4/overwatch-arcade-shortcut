9cce0209c8d2677221059f022fde187989a8ffb7
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-11-13 16:46:35 -0500

    write code

---
+++ index.js
@@ -0,0 +1,54 @@
+const got = require('got');
+const express = require('express');
+const app = express();
+
+const like = {
+  'Capture the Flag, 6v6': 4,
+  'Château Deathmatch, 8 Player FFA': 3,
+  'Competitive CTF, 6v6': 1,
+  'Competitive Deathmatch, 8 Player FFA': 3,
+  'Competitive Elimination, 3v3': 1,
+  'Competitive Elimination, 3v3 Groups Only': 1,
+  'Competitive Elimination, 6v6': 1,
+  'Competitive Open Queue, 6v6': 5,
+  'Competitive Team Deathmatch, 4v4': 7,
+  'Copa Lúcioball, 3v3': 1,
+  'Deathmatch, 8 Player FFA': 3,
+  'Elimination, 3v3': 1,
+  'Elimination, 6v6': 2,
+  'Hero Gauntlet, 8 Player FFA': 9,
+  'Horizon No Limits, 6v6': 10,
+  'Limited Duel, 1v1': 7,
+  'Low Gravity, 6v6': 3,
+  'Lúcioball, 3v3': 1,
+  'Mirrored Deathmatch, 8 Player FFA': 8,
+  'Mystery Deathmatch, 8 Player FFA': 3,
+  'Mystery Duel, 1v1': 2,
+  'Mystery Heroes, 6v6': 1,
+  'No Limits Payloads, 6v6': 10,
+  'No Limits, 6v6': 10,
+  'Petra Deathmatch, 8 Player FFA': 3,
+  'Quick Play Classic, 6v6': 0,
+  'Team Deathmatch, 4v4': 8,
+  'Total Mayhem, 6v6': 2,
+  'Yeti Hunter, 5v1': 1,
+};
+
+const getLike = (m) => {
+  if (m in like) return like[m];
+  return 11;
+};
+
+const getOW = async () => {
+  const res = await got('https://overwatcharcade.today/api/overwatch/today');
+  const b = JSON.parse(res.body);
+  const ms = Object.values(b.modes).map((m) => `${m.name}, ${m.players}`);
+  ms.sort((q, p) => getLike(p) - getLike(q));
+  return ms;
+}
+
+app.get('/', (req, res) => {
+  getOW().then((ms) => res.send(ms.join('\n')));
+});
+
+app.listen(3000);
